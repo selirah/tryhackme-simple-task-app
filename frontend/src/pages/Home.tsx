a28c1b2e3655f4798d0d96634f7900f2448ab4ed
Edward Selirah <prinzeedward@gmail.com>
2023-10-30 10:27:03 +0000

    remove unused variables in the Home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,18 +1,15 @@
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 import Header from "../layouts/Header";
 import "../css/home.css";
 import TaskTotal from "../components/tasks/TaskTotal";
 import Filter from "../components/tasks/Filter";
-import TaskItem from "../components/tasks/TaskItem";
 import { useTasks } from "../hooks/useTasks";
-import { useAuth } from "../hooks/useAuth";
 import Tasks from "../components/tasks/Tasks";
 import Loader from "../components/Loader";
 import TaskForm from "../components/tasks/TaskForm";
 
 const Home = () => {
   const tasks = useTasks();
-  const auth = useAuth();
 
   const fetchTasks = async () => {
     await tasks?.getUserTasks();
